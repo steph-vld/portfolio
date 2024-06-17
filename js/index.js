fc0334b72161cdd31672a50cb7f9c7c7ec52766f
Steph Vld <steph.vialardi@gmail.com>
2024-06-17 14:15:49 +0200

    text reveal animation

--- js/index.js
+++ js/index.js
@@ -1,35 +1,32 @@
 let selection = Splitting();
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 gsap.from(selection[0].chars, {
-    // y: 100,
-    // scaleY: 0.5,
-    // rotation: 90,
-    // opacity: 1,
-    color: "rgb(13,13,13)",
-    stagger: 0.05,
-    scrollTrigger: {
-        trigger: ".text-reveal",
-        start: "top 15%",
-        end: "bottom 100%",
-        markers: true,
-        scrub: true
-    }
-})
-const lenis = new Lenis()
+  y: 100,
+  scaleY: 0,
+  rotation: 90,
+  opacity: 0,
+  color: "rgb(13,13,13)",
+  stagger: 0.05,
+  scrollTrigger: {
+    trigger: ".text-reveal",
+    start: "top 60%",
+    end: "bottom bottom",
+    scrub: true,
+  },
+});
+const lenis = new Lenis();
 
-lenis.on('scroll', (e) => {
-  
-})
+lenis.on("scroll", (e) => {});
 
-lenis.on('scroll', ScrollTrigger.update)
+lenis.on("scroll", ScrollTrigger.update);
 
-gsap.ticker.add((time)=>{
-  lenis.raf(time * 600)
-})
+gsap.ticker.add((time) => {
+  lenis.raf(time * 400);
+});
 
-gsap.ticker.lagSmoothing(0)
+gsap.ticker.lagSmoothing(0);
 
 function toggleMenu() {
   const navLinks = document.getElementById("nav-links");
